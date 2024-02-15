4af3c8c3835e1872bb38df2282eb286938c54119
dustinkeller <dustin.w.keller@gmail.com>
2024-02-15 18:59:31 -0500

    added random gradient generation

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ var leftColor = document.getElementById("left-color");
 var rightColor = document.getElementById("right-color");
 var body = document.querySelector("body");
 var h3 = document.querySelector("h3");
+var button = document.querySelector("button");
+h3.textContent = `linear-gradient(to right, ${leftColor.value} , ${rightColor.value})`;
 
-function updateGradient() {
-    body.style.background = `linear-gradient(to right, ${leftColor.value} , ${rightColor.value})`
+function randomColor() {
+    return `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`
+}
+
+function updateGradient(event) {
+    if (event.target.id == 'random') {
+        console.log(randomColor());
+        body.style.background = `linear-gradient(to right, ${randomColor()}, ${randomColor()})`;
+    } else {
+        body.style.background = `linear-gradient(to right, ${leftColor.value} , ${rightColor.value})`;
+    }
     h3.textContent = body.style.background;
 }
 
 leftColor.addEventListener("input", updateGradient);
 
 rightColor.addEventListener("input", updateGradient);
+
+button.addEventListener("click", updateGradient);
+
